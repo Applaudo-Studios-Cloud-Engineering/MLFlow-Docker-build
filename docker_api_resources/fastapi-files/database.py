f65f3b48f262a91cc2325df9e7650831a0aff382
MarioDeLeonRecinos <mcdrecinos@gmail.com>
2023-03-15 15:45:46 -0600

    changes for helm chart deployment

--- docker_api_resources/fastapi-files/database.py
+++ docker_api_resources/fastapi-files/database.py
@@ -5,8 +5,12 @@ import os
 
 # SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
 # SQLALCHEMY_DATABASE_URL = "postgresql://mlflow:password@mlflow-mysql.cduy8wtiztlt.us-east-1.rds.amazonaws.com:3306/mlflow"
-env = os.environ['POSTGRES_ENDPOINT']
-SQLALCHEMY_DATABASE_URL = env #"postgresql://postgres:admin@localhost:5432/mlflow"
+env_host = os.environ['PGHOST'] # PGHOST PGPORT PGDATABASE PGUSER PGPASSWORD
+env_port = os.environ['PGPORT']
+env_database = os.environ['PGDATABASE']
+env_user = os.environ['PGUSER']
+env_pass = os.environ['PGPASSWORD']
+SQLALCHEMY_DATABASE_URL = 'postgresql://' + env_user + ':' + env_pass + '@' + env_host + ':' + env_port + '/' + env_database #"postgresql://postgres:admin@localhost:5432/mlflow"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, 
